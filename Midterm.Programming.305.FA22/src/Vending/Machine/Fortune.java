0671db73b8bdf816bd61e3c17751d7f63e8922a2
njanney <njanney@sandiego.edu>
2022-11-08 16:33:48 -0800

    Added test file and Fortune.java implementation

---
+++ Midterm.Programming.305.FA22/src/Vending/Machine/Fortune.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vending.Machine;
+
+/**
+ *  This is my definition of the abstract class called Fortune
+ * This class is used only in TestVendingMachine.java
+ * 
+ * <br>Name: Nicholas Janney
+ * <br>Date: Nov 8 2022
+ *
+ * @author nicho
+ */
+
+public abstract class Fortune extends Object{
+    private String fortune;
+    private String emotion;
+    
+    public abstract String getFortune();
+    public abstract String setFortune();
+    
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (getClass() != obj.getClass())
+            throw new java.lang.RuntimeException("compareTo: Empty Object!");
+        Fortune other = (Fortune) obj;
+        return (fortune.equals(other.fortune) && emotion.equals(other.emotion));
+    }
+    
+}
