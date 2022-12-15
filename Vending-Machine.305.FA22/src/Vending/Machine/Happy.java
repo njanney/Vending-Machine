161b8ab681ed1ab8d666177d1af5419406c76993
saenz-ron <55368477+saenz-ron@users.noreply.github.com>
2022-12-14 21:04:34 -0800

    midterm to vending

---
+++ Vending-Machine.305.FA22/src/Vending/Machine/Happy.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vending.Machine;
+
+/**
+ *  This is our definition of the subclass called Happy
+ * This class extends from parent class Fortune.java
+ * This class is used only in TestVendingMachine.java
+ * 
+ * <br>Names: Andrea Martinez, Nick Janney, Ronnie Saenz, Jen-Ryann Ngo-Antonio 
+ * <br>Date: Nov 8 2022
+ *
+ * @author nicho
+ */
+
+public class Happy extends Fortune{
+    private String fortune;
+    private String emotion;
+    
+    @Override
+    public String getFortune(){
+        return fortune;
+    }
+    @Override
+    public String setFortune(String fortune){
+        this.fortune = fortune;
+        return fortune;
+    }
+    
+    @Override
+    public String toString() {
+        return "";
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (getClass() != obj.getClass())
+            throw new java.lang.RuntimeException("compareTo: Empty Object!");
+        Happy other = (Happy) obj;
+        return (fortune.equals(other.fortune) && emotion.equals(other.emotion));
+    }
+    
+}
---
+++ Vending-Machine.305.FA22/src/Vending/Machine/TestVendingMachine.java
@@ -0,0 +1,23 @@
+package Vending.Machine;
+
+import java.util.Date;
+
+/**
+ *  This is the testing file for the Vending Machine
+ *  Requires Fortune.java, Happy.java, Sad.java, Angry.java.
+ *  Makes use of TextJFrame.java
+ * 
+ * <br>Names: Andrea Martinez, Nick Janney, Ronnie Saenz, Jen-Ryann Ngo-Antonio 
+ * <br>Date: Nov 8 2022
+ *
+ * @author nicho
+ */
+public class TestVendingMachine extends Object {
+
+    public static void main(String[] args) {
+        TextJFrame win = new TextJFrame("Vending Machine",
+                new Date().toString() + "\n");
+        win.Append2TextArea("Output of TestVendingMachine ...\n");
+        win.setVisible(true);
+    }
+}
